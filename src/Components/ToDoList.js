506930b009da12e4b9837d053ef91f4008b89c41
Andrew Efurhievwe <andyriles22@gmail.com>
2020-09-03 16:44:35 +0100

    Update ToDoList.js

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import ToDo from "./ToDo";
 import { List } from "@material-ui/core";
 
-function ToDoList({ toDos, toggleCompleted, removeTodo }) {
+import { GlobalContext } from "../Context/GlobalState";
+
+function ToDoList() {
+  const { toDos } = useContext(GlobalContext);
   return (
     <List>
       {toDos.map((item) => (
         <ToDo
           key={item.id}
           toDo={item}
-          toggleCompleted={toggleCompleted}
-          removeTodo={removeTodo}
         />
       ))}
     </List>
