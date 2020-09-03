a36cf9c40464f31f944dd0c93d264f3d2512b1af
Andrew Efurhievwe <andyriles22@gmail.com>
2020-09-03 16:42:51 +0100

    Update ToDoForm.js

--- src/Components/ToDoForm.js
+++ src/Components/ToDoForm.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { v4 as uuid } from "uuid";
+import { GlobalContext } from "../Context/GlobalState";
 
-function ToDoForm({ addTodo }) {
+function ToDoForm() {
   const [toDo, setTodo] = useState({ id: "", task: "", isCompleted: false });
+  const { addTodo } = useContext(GlobalContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (toDo.task.trim()) {
       addTodo({ ...toDo, id: uuid() });
+
       //reset input
       setTodo({ ...toDo, task: "" });
     }
   };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setTodo({
